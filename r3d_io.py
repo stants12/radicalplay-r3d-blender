c5af0bca8c290d9663649af8308a29e090889b7e
Mawskeeto1223 <tomaxx11@gmail.com>
2025-04-24 01:13:48 +0300

    stable 1.0, fixed exports

--- r3d_io.py
+++ r3d_io.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Radical Play 3D .r3d",
     "author": "oteek",
-    "version": (0, 1, 6),
+    "version": (1, 0, 0),
     "blender": (2, 80, 0),
     "location": "File > Import/Export > Radical Play .r3d",
     "description": "Import/Export Radical Play's .r3d files",
@@ -11,6 +11,7 @@ bl_info = {
 import bpy
 import math
 import os
+import bmesh
 from bpy_extras.io_utils import ImportHelper, ExportHelper
 from bpy.types import Operator
 from bpy.props import StringProperty
@@ -27,17 +28,11 @@ class ImportR3D(Operator, ImportHelper):
     def execute(self, context):
         fname = os.path.splitext(os.path.basename(self.filepath))[0]
         verts, normals, uvs, triplets = [], [], [], []
-        is_car = False
-        is_grouptexture = False
         def parse_floats(s): return [float(v) for v in s.split(',') if v]
         def parse_ints(s): return [int(v) for v in s.split(',') if v]
         for line in open(self.filepath):
             line = line.strip()
-            if line.startswith('car()'):
-                is_car = True
-            elif line.startswith('grouptexture('):
-                is_grouptexture = True
-            elif line.startswith('v(') and ')v' in line:
+            if line.startswith('v(') and ')v' in line:
                 vals = parse_floats(line[2:line.index(')v')])
                 verts += [(vals[i], vals[i+1], vals[i+2]) for i in range(0, len(vals), 3)]
             elif line.startswith('n(') and ')n' in line:
@@ -45,11 +40,7 @@ class ImportR3D(Operator, ImportHelper):
                 normals += [(vals[i], vals[i+1], vals[i+2]) for i in range(0, len(vals), 3)]
             elif line.startswith('t(') and ')t' in line:
                 vals = parse_floats(line[2:line.index(')t')])
-                # importer flips non-car UVs
-                if is_car or is_grouptexture:
-                    uvs += [(vals[i], vals[i+1]) for i in range(0, len(vals), 2)]
-                else:
-                    uvs += [(vals[i], 1.0 - vals[i+1]) for i in range(0, len(vals), 2)]
+                uvs += [(vals[i], vals[i+1]) for i in range(0, len(vals), 2)]
             elif line.startswith('p(') and ')p' in line:
                 idx = parse_ints(line[2:line.index(')p')])
                 # each triple: vert_idx, norm_idx, uv_idx
@@ -77,19 +68,17 @@ class ImportR3D(Operator, ImportHelper):
             for poly in mesh.polygons:
                 for li_idx, li in enumerate(poly.loop_indices):
                     ui = uv_inds[poly.index][li_idx]
-                    if 0 <= ui < len(uvs):
-                        uv_layer.data[li].uv = uvs[ui]
+                    uv_layer.data[li].uv = uvs[ui]
 
         # apply normals
         if normals and normal_inds:
-            # mesh.use_auto_smooth = True
             loop_normals = [None] * len(mesh.loops)
             for poly in mesh.polygons:
                 for li_idx, li in enumerate(poly.loop_indices):
                     ni = normal_inds[poly.index][li_idx]
-                    if 0 <= ni < len(normals):
-                        loop_normals[li] = normals[ni]
-            mesh.normals_split_custom_set([n if n else (0,0,1) for n in loop_normals])
+                    loop_normals[li] = normals[ni]
+            mesh.normals_split_custom_set(loop_normals)
+
         return {'FINISHED'}
 
 # --- Exporter ---
@@ -103,52 +92,47 @@ class ExportR3D(Operator, ExportHelper):
 
     def execute(self, context):
         obj = context.active_object
-        mesh = obj.data
-        fname = os.path.splitext(os.path.basename(self.filepath))[0]
-        is_car = obj.name.lower().startswith('car')
-        is_grouptexture = any(mat.name.startswith('g') for mat in mesh.materials)
+        # triangulate mesh copy
+        bm = bmesh.new()
+        bm.from_mesh(obj.data)
+        bmesh.ops.triangulate(bm, faces=bm.faces)
+        temp_mesh = bpy.data.meshes.new("_tmp")
+        bm.to_mesh(temp_mesh)
+        bm.free()
 
-        # gather data
-        verts = [v.co.copy() for v in mesh.vertices]
-        loop_normals = [loop.normal.copy() for loop in mesh.loops]
+        verts = [v.co for v in temp_mesh.vertices]
+        loops = temp_mesh.loops
         uvs = []
-        if mesh.uv_layers.active:
-            uv_data = mesh.uv_layers.active.data
-            uvs = [uv.uv.copy() for uv in uv_data]
+        uv_layer = temp_mesh.uv_layers.active
+        if uv_layer:
+            uvs = [uv.uv.copy() for uv in uv_layer.data]
 
         with open(self.filepath, 'w') as f:
-            if is_car: f.write('car()\n')
-            if is_grouptexture: f.write(f'grouptexture({0})\n')
-
-            # vertices
             f.write('v(')
             for v in verts:
-                f.write(f'{v.x},{v.y},{v.z},')
+                f.write(f"{v.x},{v.y},{v.z},")
             f.write(')v\n')
 
-            # normals
             f.write('n(')
-            for n in loop_normals:
-                f.write(f'{n.x},{n.y},{n.z},')
+            for l in loops:
+                n = l.normal
+                f.write(f"{n.x},{n.y},{n.z},")
             f.write(')n\n')
 
-            # UVs
             if uvs:
                 f.write('t(')
                 for uv in uvs:
-                    u, v_ = uv.x, uv.y
-                    v_out = v_ if (is_car or is_grouptexture) else 1.0 - v_
-                    f.write(f'{u},{v_out},')
+                    f.write(f"{uv.x},{uv.y},")
                 f.write(')t\n')
 
-            # faces (vertex, normal, uv indices)
             f.write('p(')
-            for poly in mesh.polygons:
+            for poly in temp_mesh.polygons:
                 for li in poly.loop_indices:
-                    lv = mesh.loops[li]
-                    f.write(f'{lv.vertex_index},{li},{li},')
+                    l = loops[li]
+                    f.write(f"{l.vertex_index},{li},{li},")
             f.write(')p\n')
 
+        bpy.data.meshes.remove(temp_mesh)
         return {'FINISHED'}
 
 # register
